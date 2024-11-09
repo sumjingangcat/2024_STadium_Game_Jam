ede17827ddabf0aefd35a1b3ae99e5100d304c5d
DevMizeKR <hwangck0827@gmail.com>
2024-11-09 22:55:03 +0900

    [Feat] Water Bar Created

--- Assets/Scripts/WaterManager.cs
+++ Assets/Scripts/WaterManager.cs
@@ -5,15 +5,25 @@ using UnityEngine.UI;
 
 public class WaterManager : MonoBehaviour
 {
+    public Image waterBar;
+
+    private HoleManager holeManager;
+    private float inputWater = 0.005f;
+    private float holeWater = 0.005f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        holeManager = FindObjectOfType<HoleManager>();
+        waterBar.fillAmount = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        waterBar.fillAmount += inputWater;
+
+        if (waterBar.fillAmount > 0) { waterBar.fillAmount -= holeManager.holes.Count * holeWater; }
+        else { Debug.Log("Zero"); }
     }
 }
