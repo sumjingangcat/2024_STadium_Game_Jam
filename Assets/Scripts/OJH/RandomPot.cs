c2b89e4651431e45694c4fe87534f67bebc738b7
sumjingangcat <oj47191@gmail.com>
2024-11-09 23:37:31 +0900

    [Feat] Add better random point picker

--- Assets/Scripts/OJH/RandomPot.cs
+++ Assets/Scripts/OJH/RandomPot.cs
@@ -2,25 +2,15 @@ using UnityEngine;
 using System.Collections;
 
 public class RandomPot : MonoBehaviour {
-    [SerializeField] private PolygonCollider2D col2D;
-    [SerializeField] private float[] rndLerpRate;
+    [SerializeField] private Transform center;
+    [SerializeField] private float radius = 0;
     [SerializeField] private GameObject checkingStar;
     [SerializeField] private GameObject checkingCircle;
-    
-    private int maxVertices = -1;
-    private Vector2[] colVerticies;
     
     
     // Start is called before the first frame update
     void Start() {
-        maxVertices = col2D.points.Length;
-        colVerticies = col2D.points;
-        /*Debug.Log($"col2D : {maxVertices}");
-        for (int i = 0; i < maxVertices; i++) {
-            GetRandomInnerPosition();
-        }*/
         
-        // drawPositionArray(col2D.points);
     }
 
     private void drawPositionArray(Vector2[] arr) {
@@ -28,26 +18,20 @@ public class RandomPot : MonoBehaviour {
             Instantiate(checkingCircle, arr[i], Quaternion.identity);
         }
     }
+
+    private Vector2 dropZVector2(Vector3 vec3) {
+        return new Vector2(vec3.x, vec3.y);
+    }
     
-    public Vector2 GetRandomInnerPosition() {
-        int rnd_num = Random.Range(0, maxVertices);
-        
-        Vector2 start = colVerticies[rnd_num];
-        Vector2 end = colVerticies[(rnd_num + maxVertices / 3) % maxVertices];
-
-        float find_rate = rndLerpRate[rnd_num % rndLerpRate.Length];
-        Vector2 find = start + (end - start) * find_rate;
-
-        Vector2 find_in_circle = find + Random.insideUnitCircle;
-        
-        // Debug Section
-        /*Debug.Log($"rnd_num : {rnd_num}, rate : {find_rate}, find position : {find}");
-        Instantiate(checkingStar, find, Quaternion.identity);*/
-        
+    public Vector2 GetRandomInnerPosition() { 
+        Random.seed = (int)Time.time;
+        Vector2 find = dropZVector2(center.position) + Random.insideUnitCircle * radius;
         return find;
     }
+    
+    
 
-    // Update is called once per frame
+// Update is called once per frame
     void Update()
     {
         
